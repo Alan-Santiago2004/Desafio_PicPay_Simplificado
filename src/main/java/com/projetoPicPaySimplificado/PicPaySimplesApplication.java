ffd815d2fbc63621c305cffbeac193074e5c6c11
Alan Santiago <alanorsantiago10@gmail.com>
2024-07-17 00:32:20 -0300

    initial configuration PicPaySimplificado

---
+++ src/main/java/com/projetoPicPaySimplificado/PicPaySimplesApplication.java
@@ -0,0 +1,13 @@
+package com.projetoPicPaySimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPaySimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPaySimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoPicPaySimplificado/PicPaySimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoPicPaySimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPaySimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
