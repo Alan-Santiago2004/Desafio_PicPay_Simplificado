a31b556de7a28b5371170665663ebfba9a69450d
Alan Santiago <alanorsantiago10@gmail.com>
2024-07-17 13:41:41 -0300

    feat: update Transaction Entity and User Entity

--- src/main/java/com/projetoPicPaySimplificado/domain/transactions/Transaction.java
+++ src/main/java/com/projetoPicPaySimplificado/domain/transactions/Transaction.java
@@ -2,18 +2,13 @@ package com.projetoPicPaySimplificado.domain.transactions;
 
 import com.projetoPicPaySimplificado.domain.users.User;
 import jakarta.persistence.*;
-import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity(name = "transaction_tb")
 @Table(name = "transaction_tb")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +21,67 @@ public class Transaction {
     @JoinColumn(name = "sender_id")
     private User receiver;
     private LocalDateTime timestamp;
+
+    public Transaction() {
+    }
+
+    public Transaction(Long id, BigDecimal amount, User sender, User receiver, LocalDateTime timestamp) {
+        this.id = id;
+        this.amount = amount;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.timestamp = timestamp;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Transaction that)) return false;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
--- src/main/java/com/projetoPicPaySimplificado/domain/users/User.java
+++ src/main/java/com/projetoPicPaySimplificado/domain/users/User.java
@@ -1,17 +1,12 @@
 package com.projetoPicPaySimplificado.domain.users;
 
 import jakarta.persistence.*;
-import lombok.*;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Entity(name = "users_tb")
 @Table(name = "users_tb")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,7 +19,96 @@ public class User {
     private String email;
     private String password;
     private BigDecimal balance;
-
     @Enumerated(EnumType.STRING)
     private TypeUser typeUser;
+
+    public User() {
+    }
+
+    public User(Long id, String firstName, String lastName, String document, String email, String password, BigDecimal balance, TypeUser typeUser) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.document = document;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+        this.typeUser = typeUser;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public TypeUser getTypeUser() {
+        return typeUser;
+    }
+
+    public void setTypeUser(TypeUser typeUser) {
+        this.typeUser = typeUser;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
