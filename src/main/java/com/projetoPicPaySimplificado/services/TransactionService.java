1eff915d9bf864f5e804670b85e0d65f2a8f33e9
Alan Santiago <alanorsantiago10@gmail.com>
2024-08-06 17:56:01 -0300

    feat: updated TransactionService

--- src/main/java/com/projetoPicPaySimplificado/services/TransactionService.java
+++ src/main/java/com/projetoPicPaySimplificado/services/TransactionService.java
@@ -1,17 +1,12 @@
 package com.projetoPicPaySimplificado.services;
 
-import com.projetoPicPaySimplificado.authorizations.AuthorizationResponse;
 import com.projetoPicPaySimplificado.domain.transactions.Transaction;
 import com.projetoPicPaySimplificado.domain.users.User;
 import com.projetoPicPaySimplificado.dtos.TransactionDto;
 import com.projetoPicPaySimplificado.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -24,7 +19,7 @@ public class TransactionService {
     private NotificationService notificationService;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private AuthorizationService authorizationService;
 
     @Autowired
     private TransactionRepository transactionRepository;
@@ -35,7 +30,7 @@ public class TransactionService {
 
         //validate transaction
         userService.validateTransaction(sender,transaction.value());
-        this.authorizeTransaction(sender,transaction.value());
+        this.authorizationService.authorizeTransaction(sender,transaction.value());
 
         //set transaction
         Transaction newTransaction = new Transaction();
@@ -52,21 +47,6 @@ public class TransactionService {
         this.notificationService.sendNotification(receiver,"transação recebida com sucesso");
 
         return newTransaction;
-    }
-
-    public boolean authorizeTransaction(User sender, BigDecimal value) throws Exception {
-        boolean isAuthorized = false;
-        ResponseEntity<AuthorizationResponse> response = restTemplate.getForEntity("https://util.devi.tools/api/v2/authorize", AuthorizationResponse.class);
-        if (response.getStatusCode() == HttpStatus.OK) {
-            AuthorizationResponse authorizationResponse = response.getBody();
-            if (authorizationResponse != null && authorizationResponse.getStatus().equals("success") && authorizationResponse.getData() != null && authorizationResponse.getData().isAuthorization()) {
-                isAuthorized = true;
-            }
-        }
-        if(!isAuthorized){
-            throw new Exception("Transação não autorizada");
-        }
-        return isAuthorized;
     }
 
     public void transactionBody(TransactionDto transactionDto,Transaction newTransaction,User sender,User receiver){
